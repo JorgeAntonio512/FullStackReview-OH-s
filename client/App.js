6a2bdc41e2ec45078726a8460d993d2a7af0674a
George Anthony Pazdral II <jorge.antonio.de.segundo@gmail.com>
2020-04-14 20:51:24 -0500

    Create App.js

--- client/App.js
+++ client/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>"This is from React App"</h1>
+      </div>
+    )
+  }
+}
+
+
+export default App;
