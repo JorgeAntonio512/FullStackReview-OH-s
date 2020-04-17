db4f33fbd111ff28e694a01932c843442fb200a4
George Anthony Pazdral II <jorge.antonio.de.segundo@gmail.com>
2020-04-16 21:02:52 -0500

    Create schema for DB

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,16 @@
+--check to see if DB exists. If yes, delete/drop
+DROP DATABASE IF EXISTS todos;
+
+--go ahead and create the DB
+CREATE DATABASE todos;
+
+--access the DB with 'USE'
+USE todos;
+
+--create a table with columns
+CREATE TABLE tasks (id INT AUTO_INCREMENT PRIMARY KEY, task VARCHAR(255));
+
+INSERT INTO tasks (task) VALUES ("take shower");
+INSERT INTO tasks (task) VALUES ("get good sleep");
+
 
